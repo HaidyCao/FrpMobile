6aa4b81384fd7e8dad910a0007beda4e4f65ba08
haidy <haidygone@gmail.com>
2024-03-22 15:28:15 +0800

    update upx_test.sh

--- upx_test.sh
+++ upx_test.sh
@@ -6,18 +6,16 @@ VERSION=${2}
 TMP_DIR=/data/local/tmp/
 
 adb push frp/bin/upx/${ARCH}_frpc ${TMP_DIR}
-RESULT=`adb shell ${TMP_DIR}${ARCH}_frpc -v`
+RESULT=`adb shell ${TMP_DIR}${ARCH}_frpc -v | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//'`
 if [[ ${RESULT} != ${VERSION} ]]; then
     echo "${RESULT}"
     echo "${ARCH}_frpc test failed"
-    exit 1
 fi
 adb push frp/bin/upx/${ARCH}_frps ${TMP_DIR}
-RESULT=`adb shell ${TMP_DIR}${ARCH}_frps -v`
+RESULT=`adb shell ${TMP_DIR}${ARCH}_frps -v | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//'`
 if [[ ${RESULT} != ${VERSION} ]]; then
     echo "${RESULT}"
     echo "${ARCH}_frps test failed"
-    exit 1
 fi
 
 echo "success"
